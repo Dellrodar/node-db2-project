2012d05ac13fd5d9bb651bc791c81e4a9ea78c8e
Dellrodar <3357168+Dellrodar@users.noreply.github.com>
2020-08-23 14:48:41 -0400

    DB migrations * Migration complete, db made. * First endpoint added.

---
+++ api/cars/cars-router.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const knex =  require("knex");
+const db = require("../../data/config");
+
+const router = express.Router();
+
+router.get("/", async (req,res, next) => {
+  try {
+    res.json(await db("cars"))
+  } catch (err) {
+    next(err);
+  }
+})
+
+module.exports = router
